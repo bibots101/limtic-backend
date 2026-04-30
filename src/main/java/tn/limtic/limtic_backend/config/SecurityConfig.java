816704c0fbb3c0e838d20e77be77928fdf447c37
Lourimi Wassim <wassimlourimi@gmail.com>
2026-04-30 19:05:35 +0100

    Update SecurityConfig.java

--- src/main/java/tn/limtic/limtic_backend/config/SecurityConfig.java
+++ src/main/java/tn/limtic/limtic_backend/config/SecurityConfig.java
@@ -120,7 +120,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "https://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+            "http://localhost:4200", 
+            "https://localhost:4200",
+            "https://limtic-frontend.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of(
             "Content-Type",
